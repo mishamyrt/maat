6e1038778d8fe34ba0162357ee832e7cf5123476
Misha <misha@myrt.co>
2017-05-29 10:03:18 +0300

    Videos oversize fix

--- maat/extensions/vimeo.php
+++ maat/extensions/vimeo.php
@@ -7,7 +7,7 @@ class MaatGroup_vimeo implements MaatGroup
         preg_match("/<p><a href=\".*vimeo\.com\/.*\">.*vimeo\.com\/(.*)<\/a><\/p>/", $content, $vimeo);
         if ($vimeo) {
             $dir = $config['cwd'].$config['imagesDirectory'].'previews/';
-            $video = '<div class="video-container"><iframe src="https://player.vimeo.com/video/'.$vimeo[1].'" width="854" height="480" frameborder="0" allowfullscreen></iframe></div>';
+            $video = '<div class="video-container"><div class="video-wrapper"><iframe src="https://player.vimeo.com/video/'.$vimeo[1].'" width="854" height="480" frameborder="0" allowfullscreen></iframe></div></div>';
             if (!file_exists($dir.$vimeo[1].'.full.jpg')) {
                 $hash = unserialize(file_get_contents("http://vimeo.com/api/v2/video/$vimeo[1].php"))[0];
                 $preview = imagecreatefromjpeg($hash['thumbnail_large']);
--- maat/extensions/youtube.php
+++ maat/extensions/youtube.php
@@ -7,7 +7,7 @@ class MaatGroup_youtube implements MaatGroup
         preg_match("/<p><a href=\".*youtube\.com\/watch\?v=.*\">.*youtube\.com\/watch\?v=(.*)<\/a><\/p>/", $content, $youtube);
         if ($youtube) {
             $dir = $config['cwd'].$config['imagesDirectory'].'previews/';
-            $video = '<div class="video-container"><iframe width="854" height="480" src="https://www.youtube.com/embed/'.$youtube[1].'" frameborder="0" allowfullscreen></iframe></div>';
+            $video = '<div class="video-container"><div class="video-wrapper"><iframe width="854" height="480" src="https://www.youtube.com/embed/'.$youtube[1].'" frameborder="0" allowfullscreen></iframe></div></div>';
             if (!file_exists($dir.$youtube[1].'.full.jpg')) {
                 $preview = imagecreatefromjpeg("https://img.youtube.com/vi/".$youtube[1]."/maxresdefault.jpg");
                 imagejpeg($preview, $dir.$youtube[1].'.full.jpg');
