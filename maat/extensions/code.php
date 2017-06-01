f54ef5dede16879a39984e511aff43f71932b75a
Misha <misha@myrt.co>
2017-06-01 16:37:23 +0300

    <code> escape

---
+++ maat/extensions/code.php
@@ -0,0 +1,18 @@
+<?php
+class MaatGroup_code implements MaatGroup
+{
+    function render($content, $config)
+    {
+        $renderedContent = $content;
+        preg_match("/<p><code>(.*)<\/code><\/p>/", $content, $quote);
+        if ($quote) {
+            // echo $quote[1];
+            $code = htmlspecialchars(str_replace('<br>', "nneewwlliinnee", $quote[1]));
+            $code = substr(str_replace("nneewwlliinnee", '<br>'."\n", $code), 5);
+            $code = substr($code, 0, -5);
+            $code = str_replace('{', "&#123;", str_replace('}',"&#125;" ,$code));
+            return '<code>'.$code.'</code>';
+        }
+        return $renderedContent;
+    }
+}
