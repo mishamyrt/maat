0f10177f8bede408f1a52d3b710ea209a58a26b0
mishamyrt <misha@myrt.co>
2018-08-16 13:36:10 +0300

    💩

--- extensions/socialnetworks.php
+++ extensions/socialnetworks.php
@@ -14,14 +14,11 @@ class MaatExtension_socialnetworks implements Maat\Extension
     }
     function render(array $group) : string
     {
-        $post = 'Твит';
+        $post = '';
         switch ($group['class']) {
             case 'twitter':
                 $json = json_decode(file_get_contents('https://publish.twitter.com/oembed?url=' . $group['class-data'][0]), true);
                 $post = $json['html'];
-                break;
-            case 'vimeo':
-                $src = 'https://player.vimeo.com/video/' . $group['class-data'][1];
                 break;
         }
         return $post;
